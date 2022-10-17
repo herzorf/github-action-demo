2a8003acc11784281d4f63188d09509a12376f31
1446450047 <1446450047@qq.com>
2022-10-17 21:09:32 +0800

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        dir: "https://herzorf.github.io/github-action-demo/"
-      }
-    }
-
-  }
+  base: '',
 })
