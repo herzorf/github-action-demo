1baa5f7a441268ffc669d19ed2707b165416c9d6
1446450047 <1446450047@qq.com>
2022-10-19 14:50:22 +0800

    修改服务器脚本

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 echo -e "---------docker Login--------"
 docker login --username=$1 hub.docker.com --password=$2
 echo -e "---------docker Stop--------"
-docker stop herzorf/vite-project:latest
+docker stop herzorf/vite-project:master
 echo -e "---------docker Rm--------"
 docker rm vite-project
-docker rmi herzorf/vite-project:latest
+docker rmi herzorf/vite-project:master
 echo -e "---------docker Pull--------"
-docker pull herzorf/vite-project:latest
+docker pull herzorf/vite-project:master
 echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 80:80 --name vite-project herzorf/vite-project:latest
+docker run --rm -d -p 80:80 --name vite-project herzorf/vite-project:master
 echo -e "---------deploy Success--------"
