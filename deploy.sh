7ecfe957581af3b1728e6f78f44da5ca3496a2c8
1446450047 <1446450047@qq.com>
2022-10-19 15:10:17 +0800

    确认实现了自动化部署

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo -e "---------docker Login--------"
-docker login --username=$1 hub.docker.com --password=$2
+docker login --username=$1  --password=$2
 echo -e "---------docker Stop--------"
 docker stop vite-project
 echo -e "---------docker Rm--------"
@@ -10,3 +10,7 @@ docker pull herzorf/vite-project:master
 echo -e "---------docker Create and Start--------"
 docker run --rm -d -p 80:80 --name vite-project herzorf/vite-project:master
 echo -e "---------deploy Success--------"
+
+
+
+docker login --username=herzorf https://hub.docker.com/ --password=love@1446450047
